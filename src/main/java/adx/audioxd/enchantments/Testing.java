167e1d3eb6bf691bda9a7e322c052e3ad8c4a847
BuildTools <unconfigured@null.spigotmc.org>
2016-03-30 00:02:12 +0200

    Updated

--- src/main/java/adx/audioxd/enchantments/Testing.java
+++ src/main/java/adx/audioxd/enchantments/Testing.java
@@ -1,6 +1,9 @@
 package adx.audioxd.enchantments;
 
 
+import adx.audioxd.customenchantmentapi.events.enchant.EEnchantEvent;
+import adx.audioxd.customenchantmentapi.events.enchant.EUnenchantEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
@@ -25,8 +28,15 @@ public class Testing extends Enchantment {
 		super(ChatColor.GOLD + "Traveler" + ChatColor.RESET, ItemType.ALL_OFF_THE_ABOVE, 40);
 	}
 
-	@EnchantmentEventHandler
-	public void onEquip(EEquipEvent event) {
+	@EnchantmentEventHandler public void enchant(EEnchantEvent event) {
+		Bukkit.broadcastMessage(ChatColor.GREEN + " enchanted" + event.getItem() + " and at level" + event.getLvl());
+	}
+
+	@EnchantmentEventHandler public void unenchant(EUnenchantEvent event) {
+		Bukkit.broadcastMessage(ChatColor.GREEN + " unenchanted" + event.getItem());
+	}
+
+	@EnchantmentEventHandler public void onEquip(EEquipEvent event) {
 		if (event.getOwner() instanceof Player) {
 			Player player = (Player) event.getOwner();
 			player.setAllowFlight(true);
@@ -36,8 +46,7 @@ public class Testing extends Enchantment {
 		event.getOwner().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 720000, event.getLvl()));
 	}
 
-	@EnchantmentEventHandler
-	public void onUnequip(EUnequipEvent event) {
+	@EnchantmentEventHandler public void onUnequip(EUnequipEvent event) {
 		if (event.getOwner() instanceof Player) {
 			Player player = (Player) event.getOwner();
 			player.setAllowFlight(false);
@@ -47,39 +56,32 @@ public class Testing extends Enchantment {
 		event.getOwner().removePotionEffect(PotionEffectType.SPEED);
 	}
 
-	@EnchantmentEventHandler
-	public void onItemInHand(EItemInOffHandEvent event) {
+	@EnchantmentEventHandler public void onItemInHand(EItemInOffHandEvent event) {
 		event.getOwner().addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 720000, event.getLvl()));
 	}
-	@EnchantmentEventHandler
-	public void onItemNotInHand(EItemNotInOffHandEvent event) {
+
+	@EnchantmentEventHandler public void onItemNotInHand(EItemNotInOffHandEvent event) {
 		event.getOwner().removePotionEffect(PotionEffectType.FAST_DIGGING);
 	}
-	
-	@EnchantmentEventHandler
-	public void onItemInHand2(EItemInMainHandEvent event) {
+
+	@EnchantmentEventHandler public void onItemInHand2(EItemInMainHandEvent event) {
 		event.getOwner().addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 720000, event.getLvl()));
 	}
 
-	@EnchantmentEventHandler
-	public void onItemNotInHand2(EItemNotInMainHandEvent event) {
+	@EnchantmentEventHandler public void onItemNotInHand2(EItemNotInMainHandEvent event) {
 		event.getOwner().removePotionEffect(PotionEffectType.ABSORPTION);
 	}
-	
-	
-	@EnchantmentEventHandler
-	public void onArrowHit(EArrowHitEvent event) {
+
+	@EnchantmentEventHandler public void onArrowHit(EArrowHitEvent event) {
 		event.getArrow().getWorld().createExplosion(event.getArrow().getLocation(), event.getLvl());
 		event.getArrow().remove();
 	}
 
-	@EnchantmentEventHandler
-	public void whenOwnerIsDamaged(EOwnerDamagedEvent event) {
+	@EnchantmentEventHandler public void whenOwnerIsDamaged(EOwnerDamagedEvent event) {
 		event.setDamage(0);
 	}
 
-	@EnchantmentEventHandler
-	public void onBlockBreak(EBlockBreakEvent event) {
+	@EnchantmentEventHandler public void onBlockBreak(EBlockBreakEvent event) {
 		event.getItem().setDurability((short) 0);
 	}
 
